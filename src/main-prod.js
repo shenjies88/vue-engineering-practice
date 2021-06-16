204473d3366bad3f6723586de348679da23fad80
shenjies88 <1961486254@qq.com>
2021-06-16 14:41:27 +0800

    feat:依赖外部CDN化

---
+++ src/main-prod.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+// import './plugins/element.js'
+import './assets/css/global.css'
+import './assets/icon/iconfont.css'
+import './config/axiosConfig.js'
+
+Vue.config.productionTip = false
+
+Vue.filter('timestampToTime', function (timestamp) {
+    //时间戳为10位需*1000，时间戳为13位的话不需乘1000
+    const date = new Date(timestamp * 1000)
+    const Y = date.getFullYear() + '-'
+    const M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-'
+    const D = date.getDate() + ' '
+    const h = date.getHours() + ':'
+    const m = date.getMinutes() + ':'
+    const s = date.getSeconds()
+    return Y + M + D + h + m + s
+})
+
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-    lintOnSave: false
+    lintOnSave: false,
+    chainWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            // 为生产环境修改配置...
+            config.entry('app').clear().add('./src/main-prod.js')
+
+            config.set('externals', {
+                vue: 'Vue',
+                'vue-router': 'VueRouter',
+                axios: 'axios',
+                lodash: '_',
+                echarts: 'echarts',
+                nprogress: 'nprogress',
+                'vue-quill-editor': 'VueQuillEditor'
+            })
+        } else {
+            // 为开发环境修改配置...
+            config.entry('app').clear().add('./src/main-dev.js')
+        }
+    }
 }
