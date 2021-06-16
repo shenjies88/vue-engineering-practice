7b117e7d51aeb562e707b3169c0c50e808f9ea20
shenjies88 <1961486254@qq.com>
2021-06-16 15:50:42 +0800

    fix:文件压缩

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const compression = require('compression')
 const app = express()
+app.use(compression())
 app.use(express.static('./dist'))
 app.listen(18000, () => {
     console.log('server run at 18000')
